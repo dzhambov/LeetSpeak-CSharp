6cd609eeaed633db5eeeaae472df4e27b9fa3d61
Hristo Dzhambov and Jieun Kang <hristo@>
2020-03-02 15:52:45 -0800

    make two tests

--- Leetspeak.Tests/ModelTests/LeetspeakTests.cs
+++ Leetspeak.Tests/ModelTests/LeetspeakTests.cs
@@ -8,9 +8,20 @@ namespace Leetspeak.Tests
   {
 
     [TestMethod]
-    public void Translate()
+    public void Translate_CreatInstanceOfLeetspeakTranslator_LeetspeakTranslato()
     {
+      LeetspeakTranslator newSentence = new LeetspeakTranslator("sentence");
+      Assert.AreEqual(typeof(LeetspeakTranslator), newSentence.GetType());
+    }
 
+    [TestMethod]
+    public void Translate_SplitWordsToChar_String()
+    {
+      string sentence = "Epicodus";
+      LeetspeakTranslator newSentence = new LeetspeakTranslator(sentence);
+      string result = newSentence.Sentence;
+
+      Assert.AreEqual(sentence, result);
     }
   }
 }
