9f227999cdf85c99702fe1c31f4759dda3e04f48
Hristo Dzhambov and Jieun Kang <jieun@>
2020-03-02 16:41:14 -0800

    add tests for leetspeak.

--- Leetspeak.Tests/ModelTests/LeetspeakTests.cs
+++ Leetspeak.Tests/ModelTests/LeetspeakTests.cs
@@ -15,13 +15,42 @@ namespace Leetspeak.Tests
     }
 
     [TestMethod]
-    public void Translate_SplitWordsToChar_String()
+    public void Translate_ReplaceEWith3_String()
     {
-      string sentence = "Epicodus";
+      string sentence = "3pic";
       LeetspeakTranslator newSentence = new LeetspeakTranslator(sentence);
-      string result = newSentence.Sentence;
+      Assert.AreEqual(sentence, newSentence.Translate("Epic"));
+    }
 
-      Assert.AreEqual(sentence, result);
+    [TestMethod]
+    public void Translate_ReplaceOWith0_String()
+    {
+      string sentence = "0rang3";
+      LeetspeakTranslator newSentence = new LeetspeakTranslator(sentence);
+      Assert.AreEqual(sentence, newSentence.Translate("orange"));
+    }
+
+    [TestMethod]
+    public void Translate_ReplaceIWith1_String()
+    {
+      string sentence = "h3ll01";
+      LeetspeakTranslator newSentence = new LeetspeakTranslator(sentence);
+      Assert.AreEqual(sentence, newSentence.Translate("helloI"));
+    }
+
+        [TestMethod]
+    public void Translate_ReplaceTWith7_String()
+    {
+      string sentence = "h3ll017";
+      LeetspeakTranslator newSentence = new LeetspeakTranslator(sentence);
+      Assert.AreEqual(sentence, newSentence.Translate("helloIt"));
+    }
+
+    public void Translate_ReplaceNotFirstLetterSWithz_String()
+    {
+      string sentence = "h3ll017z";
+      LeetspeakTranslator newSentence = new LeetspeakTranslator(sentence);
+      Assert.AreEqual(sentence, newSentence.Translate("helloIts"));
     }
   }
 }
--- Leetspeak/Models/Leetspeak.cs
+++ Leetspeak/Models/Leetspeak.cs
@@ -10,9 +10,45 @@ namespace Leetspeak.Models
     }
     public string Translate(string sentence)
     {
-      // char[] array = sentence.ToCharArray();
-      // return new string(array);
-      return sentence;
+      char[] sentenceArray = sentence.ToCharArray();
+      for(int i = 0; i < sentenceArray.Length; i++)
+      {
+        if(sentenceArray[i]== 'e' || sentenceArray[i] == 'E')
+          {
+            sentenceArray[i] = '3';
+          }
+        else if(sentenceArray[i] == 'o' || sentenceArray[i] == 'O')
+          {
+            sentenceArray[i] = '0';
+          }     
+        else if(sentenceArray[i] == 'I')
+          {
+            sentenceArray[i] = '1';
+          }
+        else if(sentenceArray[i] == 't' || sentenceArray[i] == 'T')
+          {
+            sentenceArray[i] = '7';
+          }  
+      }
+      string sentenceString = new string(sentenceArray);
+      string[] inputWordArray = sentenceString.Split(" ");
+
+      string[] convertedInput = new string[inputWordArray.Length];
+      for (int j = 0; j< inputWordArray.Length; j++)
+      {
+        char[] tempWordArray = inputWordArray[j].ToCharArray();
+        for (int i = 0; tempWordArray.Length; i++)
+        {
+          if (tempWordArray[i] == 's' || tempWordArray[i] == 'S')
+          {
+            tempWordArray[i] = 'z';
+          }
+        }
+        string reconstructWord = new string(tempWordArray);
+        convertedInput[j] = reconstructWord;
+      }
+      string result = string.Join(" ", convertedInput);
+      return result;
     }
   }
 }
